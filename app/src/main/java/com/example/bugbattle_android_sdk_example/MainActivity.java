1764efbbda1bcc5d71d28b398f72fb26597a8691
mondbaeraut <niklas@boehlerbrothers.com>
2019-08-26 10:45:11 +0200

    updated strings, updated test app, updated gradle

--- app/src/main/java/com/example/bugbattle_android_sdk_example/MainActivity.java
+++ app/src/main/java/com/example/bugbattle_android_sdk_example/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import bugbattle.io.bugbattle.BugBattle;
-import bugbattle.io.bugbattle.BugBattleActivationMethod;
+import bugbattle.io.bugbattle.controller.BugBattleActivationMethod;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        BugBattle.initialise(getApplication(), "5c41fcd794aaf5331dd4d513", BugBattleActivationMethod.SHAKE);
+        BugBattle.initialise("5d5e99fe45c384a4dd1543c3", BugBattleActivationMethod.SHAKE, getApplication());
     }
 }
