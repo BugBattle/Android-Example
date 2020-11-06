2498011ce4f7a11bfbfb8103dcf8f958773f88f2
Niklas Fessler <niklasfessler@Niklass-MacBook-Pro.local>
2020-11-06 18:05:02 +0100

    version update, color adaptions, icons done

--- app/src/main/java/com/example/bugbattle_android_sdk_example/MainActivity.java
+++ app/src/main/java/com/example/bugbattle_android_sdk_example/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.bugbattle_android_sdk_example;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import bugbattle.io.bugbattle.BugBattle;
 import bugbattle.io.bugbattle.controller.BugBattleActivationMethod;
 
@@ -12,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        BugBattle.initialise("5d5e99fe45c384a4dd1543c3", BugBattleActivationMethod.SHAKE, getApplication());
+        BugBattle.initialise("apikey", BugBattleActivationMethod.SHAKE, getApplication());
+
     }
 }
