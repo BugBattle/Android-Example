8b50962c765767b87df6af87709291d25757021d
niklasfessler <niklas@boehlerbrothers.como>
2021-01-05 14:47:33 +0100

    - updated dependency - removed api key

--- app/src/main/java/com/example/bugbattle_android_sdk_example/MainApplication.java
+++ app/src/main/java/com/example/bugbattle_android_sdk_example/MainApplication.java
@@ -12,14 +12,6 @@ public class MainApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        BugBattle.initialise("x3VJQmWjYTPSwG9b7vmKB7INaQYGI5hY", BugBattleActivationMethod.SHAKE, this);
-        try {
-            JSONObject jsonObject = new JSONObject();
-            jsonObject.put("key", "value");
-            BugBattle.attachCustomData(jsonObject);
-        } catch (Exception e) {
-
-        }
-
+        BugBattle.initialise("YOUR API KEY", BugBattleActivationMethod.SHAKE, this);
     }
 }
